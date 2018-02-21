ebc168c9c6ea256400c4759f3aecffefe4a6005d
Indrap <IndraNugraha.Putra@mitrais.com>
2018-02-21 13:36:59 +0700

    Tomcat deployment

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -2,11 +2,21 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication extends SpringBootServletInitializer {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TodoApplication.class);
 	}
 }
